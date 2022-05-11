85645db706a2bea828d951e43d73eae61ef33557
leireemh <leire.mherran@alumnos.upm.es>
2022-05-11 17:09:34 +0200

    Update proyecto.c

--- proyecto.c
+++ proyecto.c
@@ -1,114 +1,161 @@
 #include<stdio.h>
 #include<string.h>
-#define TAM_player 6
-#define TAM_MAX 300
-struct TUsuario{
-char nombre[50];
-char clave[50];
-char correo[50];
+#define TAM_player 4
+struct TJugador{
+	char nombre[50],clave[50],correo[50];
 };
-
-
 int pedirOpcion(){
 	int operacion;
 	system("cls");
-    printf("Hola,¿Quieres jugar un juego?\n\n\n");
+    printf("Hola,?Quieres jugar un juego?\n\n\n");
     printf("1-Iniciar sesion\n");
     printf("2-Registrarse\n");
     printf("3-Salir del juego\n");
     printf("Escoje una de las opciones.\n");
     scanf("%d",&operacion);
+    
     return operacion;
 }
 
 char introducirDificultad(){
 	char lvl;
-	printf("F- Nivel Facil\n");
-    printf("M- Nivel Medio\n");
-    printf("D-Nivel Dificil\n");
-    printf("E-Nivel Extremo\n");
-    printf("Introduzca en que nivel quiere jugar\n");
-    fflush(stdin);
-    scanf("%c",&lvl);
+	do {
+		printf("Introduzca en que nivel quiere jugar:\n");
+		printf("F- Nivel Facil\n");
+    	printf("M- Nivel Medio\n");
+    	printf("D-Nivel Dificil\n");
+    	fflush(stdin);
+    	scanf("%c",&lvl);
+    } 
+	while(lvl!='F' && lvl!='M' && lvl!='D');
     return lvl;
 }
 
-
-
-
 int main(){
 
-	int opc, num_player,i;
-	char nivel, correo[50], clave[50];
-	struct TUsuario usuarios[TAM_MAX];
-	FILE * pfichero;
+	int opc, num_player,i,vidas,respuesta,grup_o_s;
+	char nivel,respuesta_r,j_o_r,volver;
+	struct TJugador jugadores [6];
 
 
-    do { 
-		opc=pedirOpcion();
-        switch (opc){
-        	case 1 :
-            	fflush(stdin); //SOBRAS PUTITA
-            	printf("Para iniciar sesion:\n\n");
-         	   printf("Introduzca el correo electronico\n");
-        	    fflush(stdin);
-            	gets(correo);
-            	printf("Introduzca la clave\n");
-            	fflush(stdin);
-            	gets(clave);
-        		pfichero = fopen("usuarios.txt", "r");
-        	
-        		if (pfichero == NULL) {
-                	printf("Error en la apertura de fichero\n");
-                	return 0;
+    do {
+		system("cls");
+        opc=pedirOpcion();
+        switch (opc) {
+			case 1 :
+           		fflush(stdin);
+           		printf("Introduzca el correo del jugador\n");
+           		fflush(stdin);
+           		gets(jugadores[i].correo);
+           		printf("Introduzca clave del jugador\n");
+           		fflush(stdin);
+           		gets(jugadores[i].clave);
+
+				while(1) {
+        			do {
+            			printf("Escoja si desea jugar: J o ver el ranking: R\n");
+            			fflush(stdin);
+        				scanf("%c",&j_o_r);
+           			} 
+					while(j_o_r!='J' && j_o_r!='R');
+
+
+					switch (j_o_r) {
+    					case 'J':
+							do {
+        						printf("Escoja si quiere jugar en  solitario: 1 o en grupo: 2\n");
+        						scanf("%d",&grup_o_s);
+       						} 
+							while(grup_o_s!=1 && grup_o_s!=2);
+						
+							if(grup_o_s==1) {
+            					printf("Introduzca el nombre del jugador\n");
+                				fflush(stdin);
+                				gets(jugadores[i].nombre);
+        					}
+							else if (grup_o_s==2) {
+								
+            					printf("Introduzca el numero de jugadores en el grupo\n");
+            					fflush(stdin);
+            					scanf("%d",&num_player);
+            					
+							while(num_player>TAM_player);
+
+        					for(i=0;i<num_player;i++) {
+                				printf("Introduzca el nombre del grupo\n");
+                				fflush(stdin);
+                				gets(jugadores[i].nombre);
+            				}
+        					}
+            				do{
+            					nivel=introducirDificultad();
+
+            					switch(nivel) {
+                					case 'F':
+                    					vidas=3;
+                						printf("Responda las siguientes preguntas de manera correcta si desea salir, respondalas incorrectamente y fallecera en el camino\n");
+                						printf("\n");
+                						printf(" Cuantas patas tiene un gato?\n");
+               							fflush(stdin);
+                						scanf("%d",&respuesta);
+                
+										if (respuesta==4) {
+                    						printf("Felicidades usted ha respondido correctamente\n");
+                						}
+                						else if (respuesta!=4) {
+                    						vidas--;
+                    						printf("Lo sentimos su respuesta es incorrecta\n");
+                    						printf("Ahora le quedan %d vidas\n",vidas);
+                						}
+
+                					break;
+                					case 'M':
+                					break;
+                					case  'D':
+                					break;
+            					}
+
+                			printf("Si desea volver a jugar preione S, sino presione cualquier otra letra\n");
+                			fflush(stdin);
+                			scanf("%c",&respuesta_r);
+            				}
+							while(respuesta_r =='S');
+            				break;
+            
+						case 'R':
+            			break;
+					}
+    	
+        		printf("Si deseas volver a la pagina anterior presione: V, para salir presoine cualqier otra letra\n");
+        		fflush(stdin);
+        		scanf("%c",&volver);
+
+        		if(volver!= 'V') {
+        			break;
         		}
-        	
-        		i=0;
-        		while (fscanf(pfichero, "%s %s %s", usuarios[i].nombre, usuarios[i].correo, usuarios[i].clave) != EOF) {
-                	printf("%s %s %s\n", usuarios[i].nombre, usuarios[i].correo, usuarios[i].clave);
-                	i++;
-         		}
-        		fclose(pfichero);
-
-        		for (i=0; i<TAM_MAX; i++) {
-                	if (strcmp(correo, usuarios[i].correo) == 0)  {
-                        printf("Usuario ya existente");
-                	}	
 				}
-
-            	do{
-            	printf("Introduzca el numero de jugadores\n");
-            	fflush(stdin);
-            	scanf("%d",&num_player);
-            	}while(num_player>TAM_player);
-            	
-            	for(i=0;i<num_player;i++){
-                	printf("Introduzca el nombre del jugador %d\n",i);
-                	fflush(stdin);
-                	gets(usuarios[i].nombre);
-            	}
-            	
-            	do{
-            		nivel=introducirDificultad();
-            	}while(nivel!='F' && nivel!='M' && nivel!='D' && nivel!='E');
-            	break ;
+        		break ;
 
         	case 2 :
-            	fflush(stdin);
+                fflush(stdin);
             	printf("Introduzca nombre del jugador\n");
             	fflush(stdin);
-            	gets(usuarios[i].nombre);
+            	gets(jugadores[i].nombre);
             	printf("Introduzca el correo del jugador\n");
             	fflush(stdin);
-            	gets(usuarios[i].correo);
+            	gets(jugadores[i].correo);
             	printf("Introduzca clave del jugador\n");
             	fflush(stdin);
-            	gets(usuarios[i].clave);
+            	gets(jugadores[i].clave);
         	break ;
         }
         system("pause");
-        
-    }while(opc!=3||opc<1||opc>3);
-    
+
+    }    
+	while(opc!=3||opc<1||opc>3);
+
+
+
+
 return 0;
 }
